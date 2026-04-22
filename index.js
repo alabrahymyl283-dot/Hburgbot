dc8f41788fc01d738d19ba635b78a246e6398681
alabrahymyl283-dot <alabrahymyl283@gmail.com>
2026-04-22 16:09:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ async function askGemini(userText, fromName) {
     })
   });
   console.log("GEMINI RESPONSE:", JSON.stringify(res).slice(0, 200));
-  if (!res.candidates) return "اه اه اه ";
+  if (!res.candidates) return "العب بمالات عمك ";
   return res.candidates[0].content.parts[0].text || "...";
 }
 
